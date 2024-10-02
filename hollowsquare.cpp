25bead3095bf05c36eb582ec28576c0058fad752
Shivam Aggarwal <155300558+hackers470@users.noreply.github.com>
2024-10-02 22:21:11 +0530

    Update hollowsquare.cpp

--- hollowsquare.cpp
+++ hollowsquare.cpp
@@ -1,1 +1,24 @@
 
+//HOLLOW SQUARE
+#include<iostream>
+using namespace std;
+#include<stdlib.h>
+int main()
+{int length;
+ cout<<"Enter a number to print from 1 to 20";
+ cin>>length;
+ 
+ if(length<1 || length>20)
+   {cout<<"Enter valid length";
+    exit(0);
+    }
+    for(int i=0;i<length;i++)
+    {for(int j=0;j<length;j++)
+        {if(j==0 || j==length-1 || i==0 || i==length-1)
+          {cout<<"*";}
+        else
+           {cout<<" ";}
+        }
+        cout<<"\n";
+    }
+}
